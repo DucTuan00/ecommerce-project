658d8b4e6e40abb878894fe26fd36aa22b58c7aa
hoang.manh.toan <hoang.manh.toan@mqsolutions.com.vn>
2024-09-22 17:26:28 +0700

    update:Layout

--- client/home/home.js
+++ client/home/home.js
@@ -1,23 +1,23 @@
-// // Hàm để lấy giá trị của cookie theo tên
-// function getCookie(name) {
-//     const value = `; ${document.cookie}`;
-//     const parts = value.split(`; ${name}=`);
-//     if (parts.length === 2) return parts.pop().split(';').shift();
-//     return null;
-// }
+// Hàm để lấy giá trị của cookie theo tên
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return null;
+}
 
-// // Kiểm tra token khi trang load
-// window.onload = function() {
-//     const token = getCookie('csrftoken'); // Lấy cookie có tên 'csrftoken'
+// Kiểm tra token khi trang load
+window.onload = function() {
+    const token = getCookie('token'); // Lấy cookie có tên 'csrftoken'
 
-//     if (!token) {
-//         // Chuyển hướng tới trang login nếu không có token
-//         window.location.href = '../login/login.html';
-//     } else {
-//         // Nếu có token, bắt đầu slideshow
-//         initSlideshow();
-//     }
-// };
+    if (!token) {
+        // Chuyển hướng tới trang login nếu không có token
+        window.location.href = '../login/login.html';
+    } else {
+        // Nếu có token, bắt đầu slideshow
+        initSlideshow();
+    }
+};
 
 // JavaScript to handle the slideshow functionality
 let slideIndex = 0;
@@ -38,7 +38,7 @@ function initSlideshow() {
     setInterval(showSlides, 3000);
 }
 
-document.querySelector('.add-product').addEventListener('click', function() {
-    // Điều hướng đến trang addProduct.html
+
+document.querySelector('.add-product-icon').addEventListener('click', function() {
     window.location.href = '../addProduct/addProduct.html';
 });
