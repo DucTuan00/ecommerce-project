274697328c118b4f0cc3f3d969b07dfc6131f08e
hoang.manh.toan <hoang.manh.toan@mqsolutions.com.vn>
2024-10-18 16:05:19 +0700

    feat: Thêm tính năng  thống kê doanh thu hàng tháng

--- server-nodejs/server.js
+++ server-nodejs/server.js
@@ -4,6 +4,7 @@ const categoryRoutes = require('./routes/categoryRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const statisticalRoutes = require('./routes/statisticalRouter');
 const cors = require('cors'); 
 
 const app = express();
@@ -21,6 +22,7 @@ app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/statistical', statisticalRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
